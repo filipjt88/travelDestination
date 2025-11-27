fb4ca5d3a12681ef7b74bad83b2c9cab9cc0c6ff
filipjt88 <filip88ks@gmail.com>
2025-11-27 11:18:16 +0100

    added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import 'leaflet/dist/leaflet.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
